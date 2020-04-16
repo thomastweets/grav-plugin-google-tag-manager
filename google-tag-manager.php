7705b5342168f1713cf0967c27cb7393beadc41c
Thomas Emmerling <thomas.emmerling@lingoking.com>
2020-04-16 10:51:04 +0200

    Add environment and authentification key to plugin.

--- google-tag-manager.php
+++ google-tag-manager.php
@@ -37,22 +37,28 @@ class GoogleTagManagerPlugin extends Plugin
         $trackingId = trim($this->grav['config']->get('plugins.google-tag-manager.trackingId', ''));
         if (empty($trackingId)) return;
 
+        // Get optional parameters
+        $auth = trim($this->grav['config']->get('plugins.google-tag-manager.auth', ''));
+        $env = trim($this->grav['config']->get('plugins.google-tag-manager.env', ''));
+
         if (!$this->isTheRightDomain()) return;
 
         // Embed Google Tag Manager script
-        $this->grav['assets']->addInlineJs($this->generateTrackingCode($trackingId), null);
+        $this->grav['assets']->addInlineJs($this->generateTrackingCode($trackingId, $auth, $env), null);
     }
     /**
      * Generate GTM Code for embeded.
      * @param string $trackingId Google Tag Manager Tracking ID
+     * @param string $auth Google Tag Manager authetification
+     * @param string $env Google Tag Manager Environment
      */  
-    private function generateTrackingCode($trackingId) {
+    private function generateTrackingCode($trackingId, $auth, $env) {
         $code = <<<EOF
 <!-- Google Tag Manager -->
 (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+'https://www.googletagmanager.com/gtm.js?id='+i+dl+ '&gtm_auth=$auth&gtm_preview=$env&gtm_cookies_win=x';f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','{$trackingId}');
 <!-- End Google Tag Manager -->
 EOF;
